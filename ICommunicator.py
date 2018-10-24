71e61badde2108805f6842949548c5fb2dffb43a
Andres Anania <aanania@lsst.org>
2018-10-24 13:25:46 -0300

    added new functions to inerface.

--- ICommunicator.py
+++ ICommunicator.py
@@ -1,50 +1,50 @@
 from abc import ABC, abstractmethod
 
 class ICommunicator(ABC):
-    """Abstract class to handle different communication protocols. 
-    ----------
+	"""Abstract class to handle different communication protocols.
+	----------
 
-    Notes
-    -----"""
+	Notes
+	-----"""
 		
-    @abstractmethod
-    def connect(self
+	@abstractmethod
+	def connect(self):
 		"""Connect
 		"""
-        pass
+		pass
 		
-    @abstractmethod
-    def disconnect(self):
-        pass
+	@abstractmethod
+	def disconnect(self):
+		pass
 		
-    @abstractmethod
-    def getMessage(self):
+	@abstractmethod
+	def getMessage(self):
 		"""Get message from currect connection 
 	
 		Returns errorCode, message, errorMessage
 		"""
-        pass
+		pass
 		
-    @abstractmethod
-    def sendMessage(self, message):
+	@abstractmethod
+	def sendMessage(self, message):
 		"""Send message from currect connection  
 	
 		Returns errorCode, errorMessage
 		"""
-        pass
+		pass
 		
-    @abstractmethod
-    def reconnect(self):
+	@abstractmethod
+	def reconnect(self):
 		"""Reconnect 
 	
 		Returns errorCode, errorMessage
 		"""
-        pass
+		pass
 		
-    @abstractmethod
-    def isConnected(self):
+	@abstractmethod
+	def isConnected(self):
 		"""Returns an internal parameter to check if it is connected. 
 	
 		Returns isConnected: True or False
 		"""
-        pass
+		pass
