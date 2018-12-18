edf4ab13f2e8e0283d9727390be8f807338de433
Andres Anania <AAnania@lsst.org>
2018-12-18 09:53:20 -0500

    Added package dir to setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,9 @@ setup(
     version="0.1.0",
     author="Andres Anania",
     author_email="aanania@lsst.org",
-    packages=["lsst.ts.pythonCommunicator"]
+    description="Package to manage TCP and Serial communication",
+    url="https://github.com/lsst-ts/pythonCommunicator",
+    install_requires=["asyncio","pySerial"],
+    packages=["lsst.ts.pythonCommunicator"],
+    package_dir={"":"python"}
 )
